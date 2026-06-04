8dd7f61cd78d1265e46f1801a674b5a8a7377034
Sushant Ramkrushna Telgote <sushanttelgote4@gmail.com>
2026-06-04 16:08:32 +0530

    Update app.py

--- Downloads/Rag-Knowledge-Worker-main/Rag-Knowledge-Worker-main/app.py
+++ Downloads/Rag-Knowledge-Worker-main/Rag-Knowledge-Worker-main/app.py
@@ -311,7 +311,7 @@ def main():
     ui.launch(
     server_name="0.0.0.0",
     server_port=int(os.environ.get("PORT", 7860)),
-    share=True
+    share=False
 )
 
 # ---------------- RUN ----------------
